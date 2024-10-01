b2e50ab6907d671c87dc939c7478136be533ab38
g g <gabedotgutierrez@gmail.com>
2024-09-30 22:58:33 -0400

    stumped at post

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -12,7 +12,23 @@ router.get('/', async (req, res) => {
     const allRecipes = await Recipe.find({});
     console.log(allRecipes) //all the recipes in console
     res.render('recipes/index.ejs', {recipes: allRecipes});
+});
+
+
+router.post('/', async (req, res) => {
+    try {
+      const newRecipe = new Recipe(req.body);
+      newRecipe.owner = req.session.user._id
+      await newRecipe.save();
+      // Redirect to recipe index or show page
+    } catch (error) {
+      // Handle errors
+    }
   });
-  
+
+router.get('/new', async (req,res)=>{
+    // res.send("we've hit the new page")
+    res.render('recipes/new.ejs')
+})
 
 module.exports = router;
